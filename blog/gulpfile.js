a7968928115eea2fe52eb09e94f437d5c4c73068
one XUNLEIer <zhengfeijie@xunlei.com>
2018-07-12 15:55:10 +0800

    mini gulp

--- blog/gulpfile.js
+++ blog/gulpfile.js
@@ -1,28 +1,26 @@
 const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
 const cleanCSS = require('gulp-clean-css')
 const uglify = require('gulp-uglify')
-const htmlmin = require('gulp-htmlmin')
-const htmlclean = require('gulp-htmlclean')
 const imagemin = require('gulp-imagemin')
 
 // 压缩html
 gulp.task('minify-html', function () {
   return gulp.src(['./public/**/*.html'])
-    .pipe(htmlclean())
     .pipe(htmlmin({
-      removeComments: true,
-      minifyJS: true,
+      collapseWhitespace: true,
       minifyCSS: true,
-      minifyURLs: true
+      minifyJS: true,
+      removeComments: true
     }))
     .pipe(gulp.dest('./public'))
 })
 
 // 压缩css
 gulp.task('minify-css', function () {
-  return gulp.src(['./public/**/*.css'])
+  return gulp.src(['./public/css/**/*.css'])
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./public'))
+    .pipe(gulp.dest('./public/css'))
 })
 
 // 压缩js
@@ -35,17 +33,20 @@ gulp.task('minify-js', function () {
 // 压缩图片
 gulp.task('minify-images', function () {
   return gulp.src(['./public/**/*.png', './public/**/*.jpg', './public/**/*.gif', './public/**/*.svg'])
-    .pipe(imagemin(
-      [imagemin.gifsicle({ 'optimizationLevel': 3 }),
+    .pipe(imagemin([
+        imagemin.svgo(),
+        imagemin.gifsicle({ 'optimizationLevel': 3 }),
         imagemin.jpegtran({ 'progressive': true }),
-        imagemin.optipng({ 'optimizationLevel': 7 }),
-        imagemin.svgo()],
-      { 'verbose': true }
+        imagemin.optipng({ 'optimizationLevel': 7 })
+      ], { 'verbose': true }
     ))
     .pipe(gulp.dest('./public'))
 })
 
 // 默认任务
 gulp.task('default', [
-  'minify-html', 'minify-css', 'minify-js', 'minify-images'
+  'minify-html',
+  'minify-css',
+  'minify-js',
+  'minify-images'
 ])
